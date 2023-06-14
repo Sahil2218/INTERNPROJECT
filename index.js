c20c306ece7273f74b78f8755338a43795b65e3a
Sahil2218 <135003194+Sahil2218@users.noreply.github.com>
2023-06-14 22:58:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const app = express();
 app.get("/",function(req,res){
 res.sendFile(__dirname + "/index.html"  );
 });
-app.use(express.static('INTERN PORGCTS'));
-
 app.listen(3000,function(){
     console.log("sever is running in port 3000");
 });
